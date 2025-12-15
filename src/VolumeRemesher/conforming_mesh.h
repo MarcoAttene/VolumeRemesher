29390c95e6d4de0d92d2b8d0c21400c855796eb9
Daniel Zint <daniel.zint@mail.de>
2025-12-15 11:56:34 -0500

    Update VolumeRemesher code.

--- src/VolumeRemesher/conforming_mesh.h
+++ src/VolumeRemesher/conforming_mesh.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef _CONFORMING_MESH_
+#define _CONFORMING_MESH_
 
 #include "delaunay.h"
 
@@ -7,41 +8,56 @@
 #define CONSTR_B 1
 
 namespace vol_rem {
-
 // half-edges struct
-struct half_edge_t {
-  uint32_t endpts[2]; // endpoints of a constraint(triangle) edge,
-                      // be sure that endpts[0] < endpts[1].
-  uint32_t tri_ind;   // id_number of the constraint: tri_vertices[i]/3, where
-                      // tri_vertices[i]=endpts[0] or tri_vertices[i]=endpts[1].
+struct half_edge_t
+{
+    uint32_t endpts[2]; // endpoints of a constraint(triangle) edge,
+    // be sure that endpts[0] < endpts[1].
+    uint32_t tri_ind; // id_number of the constraint: tri_vertices[i]/3, where
+    // tri_vertices[i]=endpts[0] or tri_vertices[i]=endpts[1].
 };
 
 // constraints are triangles
-class constraints_t {
+class constraints_t
+{
 public:
-  uint32_t *tri_vertices;
-  uint32_t num_triangles;
-  uint32_t *constr_group;
-  uint32_t num_virtual_triangles; // virtual constraints are indexed
-                                  // from num_triangles - num_virtual_triangles
-                                  // to num_triangles-1.
+    uint32_t* tri_vertices;
+    uint32_t num_triangles;
+    uint32_t* constr_group;
+    uint32_t num_virtual_triangles; // virtual constraints are indexed
+    // from num_triangles - num_virtual_triangles
+    // to num_triangles-1.
 
-  constraints_t()
-      : tri_vertices(NULL), num_triangles(0), constr_group(NULL),
-        num_virtual_triangles(0){};
-  ~constraints_t() {
-    if (tri_vertices)
-      free(tri_vertices);
-    if (constr_group)
-      free(constr_group);
-  }
+    constraints_t()
+        : tri_vertices(NULL)
+        , num_triangles(0)
+        , constr_group(NULL)
+        , num_virtual_triangles(0) {};
+    ~constraints_t()
+    {
+        if (tri_vertices) free(tri_vertices);
+        if (constr_group) free(constr_group);
+    }
 };
 
-void fill_half_edges(const constraints_t *constraints, half_edge_t *half_edges);
-void sort_half_edges(half_edge_t *half_edges, uint32_t num_half_edges);
-uint32_t place_virtual_constraints(TetMesh *mesh, constraints_t *constraints,
-                                   half_edge_t *half_edges);
-void insert_constraints(TetMesh *, constraints_t *, uint32_t *, uint32_t **,
-                        uint32_t *, uint32_t **, uint32_t *, uint32_t **,
-                        uint32_t *, uint32_t **, uint32_t *, uint32_t **);
+
+void fill_half_edges(const constraints_t* constraints, half_edge_t* half_edges);
+void sort_half_edges(half_edge_t* half_edges, uint32_t num_half_edges);
+uint32_t
+place_virtual_constraints(TetMesh* mesh, constraints_t* constraints, half_edge_t* half_edges);
+void insert_constraints(
+    TetMesh*,
+    constraints_t*,
+    uint32_t*,
+    uint32_t**,
+    uint32_t*,
+    uint32_t**,
+    uint32_t*,
+    uint32_t**,
+    uint32_t*,
+    uint32_t**,
+    uint32_t*,
+    uint32_t**);
 } // namespace vol_rem
+
+#endif
--- src/VolumeRemesher/embed.h
+++ src/VolumeRemesher/embed.h
@@ -1,7 +1,5 @@
-#pragma once
-
-#include "BSP.h"
 #include <vector>
+#include "BSP.h"
 
 namespace vol_rem {
 /// <summary>
@@ -15,41 +13,33 @@ namespace vol_rem {
 /// </summary>
 ///
 /// INPUT (T and M)
-/// <param name="tri_vrt_coords">Vertex coordinates of T
-/// (x1,y1,z1,x2,y2,z2,...,xn,yn,zn)</param> <param
-/// name="triangle_indexes">Triangle indexes of T (t1_v1, t1_v2, t1_v3, t2_v1,
-/// t2_v2, t2_v3, ..., tn_v1, tn_v2, tn_v3)</param> <param
-/// name="tet_vrt_coords">Vertex coordinates of M
-/// (x1,y1,z1,x2,y2,z2,...,xn,yn,zn)</param> <param name="tet_indexes">Tet
-/// indexes of M (t1_v1, t1_v2, t1_v3, t1_v4, t2_v1, t2_v2, t2_v3, t2_v4,
-/// ...)</param>
+/// <param name="tri_vrt_coords">Vertex coordinates of T (x1,y1,z1,x2,y2,z2,...,xn,yn,zn)</param>
+/// <param name="triangle_indexes">Triangle indexes of T (t1_v1, t1_v2, t1_v3, t2_v1, t2_v2, t2_v3, ..., tn_v1, tn_v2, tn_v3)</param>
+/// <param name="tet_vrt_coords">Vertex coordinates of M (x1,y1,z1,x2,y2,z2,...,xn,yn,zn)</param>
+/// <param name="tet_indexes">Tet indexes of M (t1_v1, t1_v2, t1_v3, t1_v4, t2_v1, t2_v2, t2_v3, t2_v4, ...)</param>
 ///
 /// OUTPUT (M')
-/// <param name="vertices">Vertex coordinates of M'
-/// (x1,y1,z1,x2,y2,z2,...,xn,yn,zn)</param> <param name="facets">Polygonal
-/// facets in M'. A facet with n vertices is a sequence (n, p_v1, p_v2, ...,
-/// p_vn). The first number in the sequence is the number of vertices in the
-/// polygon, whereas the subsequent n numbers are its vertex indexes.</param>
-/// <param name="cells">Polyhedral cells in M'. A polyhedron with n facets is a
-/// sequence (n, p_f1, p_f2, ..., p_fn). The first number in the sequence is the
-/// number of facets bounding the polyhedron, whereas the subsequent n numbers
-/// are its facet indexes.</param> <param name="facets_on_input">Indexes of
-/// facets that overlap with T.</param>
+/// <param name="vertices">Vertex coordinates of M' (x1,y1,z1,x2,y2,z2,...,xn,yn,zn)</param>
+/// <param name="facets">Polygonal facets in M'. A facet with n vertices is a sequence (n, p_v1, p_v2, ..., p_vn).
+/// The first number in the sequence is the number of vertices in the polygon, whereas the subsequent n numbers are its
+/// vertex indexes.</param>
+/// <param name="cells">Polyhedral cells in M'. A polyhedron with n facets is a sequence (n, p_f1, p_f2, ..., p_fn).
+/// The first number in the sequence is the number of facets bounding the polyhedron, whereas the subsequent n numbers are its
+/// facet indexes.</param>
+/// <param name="facets_on_input">Indexes of facets that overlap with T.</param>
 ///
 /// <param name="verbose">Set to TRUE to enable verbosity.</param>
 
 void embed_tri_in_poly_mesh(
-    const std::vector<double> &tri_vrt_coords,
-    const std::vector<uint32_t> &triangle_indexes,
-    const std::vector<double> &tet_vrt_coords,
-    const std::vector<uint32_t> &tet_indexes,
-    std::vector<bigrational> &vertices, std::vector<uint32_t> &facets,
-    std::vector<uint32_t> &cells,
-    std::vector<std::array<uint32_t, 4>> &out_tets,
-    std::vector<uint32_t> &final_tets_parent,
-    std::vector<uint32_t> &facets_on_input,
-    std::vector<bool> &cells_with_faces_on_input,
-    std::vector<std::vector<uint32_t>> &final_tets_parent_faces, bool verbose);
+    const std::vector<double>& tri_vrt_coords,
+    const std::vector<uint32_t>& triangle_indexes,
+    const std::vector<double>& tet_vrt_coords,
+    const std::vector<uint32_t>& tet_indexes,
+    std::vector<bigrational>& vertices,
+    std::vector<uint32_t>& facets,
+    std::vector<uint32_t>& cells,
+    std::vector<uint32_t>& facets_on_input,
+    bool verbose);
 
 //
 // TO DO: CONSIDER INPUT POINTS AND SEGMENTS
